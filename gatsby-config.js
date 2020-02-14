1cf2f1b52426ba95185712254f84d7d90a900a05
Jesse Florig <jesseflorig@gmail.com>
2020-02-14 02:31:14 -0500

    created home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,13 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `hackchur.ch`,
+        icon: `static/favicon.png`,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,30 @@
 import React from "react"
+import { Helmet } from "react-helmet"
+import HackChurch from "./hackchurch"
 
-export default () => <div>Hello world!</div>
+const containerStyle = {
+  position: `absolute`,
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  display: `flex`,
+  alignItems: `center`,
+  justifyContent: `center`,
+  backgroundColor: `#bf2020`,
+}
+
+export default () => {
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>hackchur.ch</title>
+        <link rel="canonical" href="https://hackchur.ch" />
+      </Helmet>
+      <div style={containerStyle}>
+        <HackChurch />
+      </div>
+    </>
+  )
+}
